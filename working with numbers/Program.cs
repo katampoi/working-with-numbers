002a885c83b25d66750ba956edccc875eda39b4b
KATAMPOI <KATAMPOI@EUCROLA>
2024-09-04 21:52:09 +0300

    Add project files.

---
+++ working with numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace workingNumbers
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            num++;
+
+            Console.WriteLine(5+8);
+            Console.WriteLine(8-5);
+            Console.WriteLine(8*5);
+            Console.WriteLine(10/5);
+            Console.WriteLine(5%2);
+            Console.WriteLine(5/2.0);
+            Console.WriteLine(num);
+            Console.WriteLine(Math.Pow(3,2));
+            Console.WriteLine(Math.Sqrt(49));
+            Console.WriteLine(Math.Max(4,90));
+            Console.WriteLine(Math.Min(4,90));
+            Console.WriteLine(Math.Round(4.3));
+            Console.WriteLine(Math.Round(4.6));
+
+            Console.ReadLine();
+        }
+    }
+}
